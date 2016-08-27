e3941b73254f31472d36d0ef4d82fe2e33197be0
lourosas <lourosas@gmail.com>
2016-08-27 06:28:19 -0700

    Add files via upload

---
+++ RaspberryPiWeatherStationCode/bin/ASimpleServer2.java
@@ -0,0 +1,55 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import rosas.lou.weatherclasses.Database;
+
+public class ASimpleServer2{
+   private DatagramSocket socket;
+
+   public static void main(String[] args){
+      new ASimpleServer2();
+   }
+
+   public ASimpleServer2(){
+      try{
+         this.socket = new DatagramSocket(9302);//Set the port high
+         this.waitForPackets();
+      }
+      catch(Exception e){
+         e.printStackTrace();
+         System.exit(1);
+      }
+   }
+
+   public void waitForPackets(){
+      while(true){
+         try{
+            byte data[] = new byte[500];
+            DatagramPacket receivePacket =
+                                new DatagramPacket(data, data.length);
+            this.socket.receive(receivePacket);
+            System.out.println(receivePacket.getAddress());
+            System.out.println(receivePacket.getPort());
+            System.out.println(receivePacket.getLength());
+            String received = new String(receivePacket.getData(), 
+                                         0,
+                                         receivePacket.getLength());
+            System.out.println(received);
+            if(received.toUpperCase().equals("TEMPERATURE")){
+               this.findTemperatureData();
+            }
+         }
+         catch(IOException ioe){ ioe.printStackTrace(); }
+      }
+   }
+
+   public void findTemperatureData(){
+      final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+      final String DB_URL="jdbc:mysql://localhost:3306/weatherdata";
+      final String USER = "root";
+      final String PASS = "password";
+      String select = new String("SELECT * from temperaturedata;");
+      System.out.println(select);
+   }
+}
