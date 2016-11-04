06585114f6d1431de24e6c127ff577eec36d2655
lourosas <lourosas@gmail.com>
2016-11-03 19:01:19 -0700

    Add files via upload

--- RaspberryPiWeatherStationCode/bin/ASimpleClient2.java
+++ RaspberryPiWeatherStationCode/bin/ASimpleClient2.java
@@ -11,35 +11,44 @@ public class ASimpleClient2{
    }
 
    public ASimpleClient2(){
+      DatagramPacket sendPacket    = null;
+      DatagramPacket receivePacket = null;
       try{
          this.socket = new DatagramSocket();
-         String message = new String("Temperature");
+         //String message = new String("Temperature");
+         //String message = new String("Temperature");
+         String message = new String("Select * from  heatindexdata ");
+         message += "WHERE month = 'October' AND day = '30'";
          byte data[] = message.getBytes();
-         byte[] receiveData = new byte[64];
          byte[] addr = new byte[]
-                             {(byte)192,(byte)168,(byte)1,(byte)130};
+                             {(byte)192,(byte)168,(byte)1,(byte)112};
          InetAddress iNetAddr = InetAddress.getByAddress(addr);
-         DatagramPacket sendPacket = new DatagramPacket(data,
-            data.length, iNetAddr, 9301);
-         this.socket.send(sendPacket);
-         System.out.println("Poop");
-         DatagramPacket receivePacket =
+         while(true){
+            byte[] receiveData = new byte[64];
+            sendPacket = new DatagramPacket(data,
+                                            data.length,
+                                            iNetAddr,
+                                            19000);
+            this.socket.send(sendPacket);
+            System.out.println("Poop");
+            receivePacket =
                  new DatagramPacket(receiveData, receiveData.length);
-         this.socket.receive(receivePacket);
-         String output = new String(receivePacket.getData());
-         int size = Integer.parseInt(output.trim());
-         System.out.println(size);
-         boolean toPrint = true;
-         int i = 0;
-         do{
             this.socket.receive(receivePacket);
-            System.out.println(i + ": "+receivePacket.getAddress()+
-             ", " + receivePacket.getLength());
-            output = new String(receivePacket.getData());
-            System.out.println(output);
-            ++i;
-            toPrint = (i < size);
-         }while(true);
+            String output = new String(receivePacket.getData());
+            int size = Integer.parseInt(output.trim());
+            int i = 0;
+            while(i < size){
+               this.socket.receive(receivePacket);
+               System.out.println(i+ ": "+receivePacket.getAddress()+
+                                   ", " + receivePacket.getLength());
+               output = new String(receivePacket.getData());
+               System.out.println(output);
+               ++i;
+            }
+            System.out.println(size);
+            try{ Thread.sleep(300000); }
+            catch(InterruptedException ie){}
+         }
       }
       catch(Exception e){ e.printStackTrace(); }
    }
