ccd31950e3052571ed02d64efe886e3308c89b7c
lourosas <lourosas@gmail.com>
2017-11-22 12:51:27 -0700

    11/22/2017-1

--- RaspberryPiWeatherStationCode/bin/TestWeatherServer.java
+++ RaspberryPiWeatherStationCode/bin/TestWeatherServer.java
@@ -11,31 +11,49 @@ public class TestWeatherServer{
    }
 
    public TestWeatherServer(){
-      Wunderground   wg      = new Wunderground();
+      //Wunderground   wg      = new Wunderground();
       WeatherStation station = new WeatherStation();
-      WeatherServer  server  = new WeatherServer(19000);
       //server.registerWithAWeatherStation(new WeatherStation());
-      server.registerWithAWeatherStation(station);
-      station.addBarometerObserver(wg);
-      station.addCalculatedObserver(wg);
-      station.addHumidityObserver(wg);
-      station.addTemperatureObserver(wg);
-      station.addExtremeObserver(wg);
-      Thread thread = new Thread(server);
-      thread.start();
-      Thread nextThread = new Thread(wg);
-      nextThread.start();
+      //station.addBarometerObserver(wg);
+      //station.addCalculatedObserver(wg);
+      //station.addHumidityObserver(wg);
+      //station.addTemperatureObserver(wg);
+      //station.addExtremeObserver(wg);
+      //Thread nextThread = new Thread(wg);
+      //nextThread.start();
       try{
+         WeatherServer  server  = new WeatherServer(19000);
+         server.registerWithAWeatherStation(station);
+         Thread thread = new Thread(server);
+         thread.start();
+         /*
          WeatherDataHandler wdh = new WeatherDataHandler();
          station.addTemperatureObserver(wdh);
          station.addHumidityObserver(wdh);
          station.addBarometerObserver(wdh);
          station.addCalculatedObserver(wdh);
          station.addExtremeObserver(wdh);
+         */
+         //BaseWeatherHandler bwh = new BaseWeatherHandler();
+         //Declared vs Runtime
+         BaseWeatherHandler bwh = new CurrentWeatherHandler();
+         BaseWeatherHandler dwh = new DailyWeatherHandler();
+         station.addTemperatureObserver(bwh.weatherDataSubscriber());
+         station.addHumidityObserver(bwh.weatherDataSubscriber());
+         station.addBarometerObserver(bwh.weatherDataSubscriber());
+         station.addCalculatedObserver(bwh.weatherDataSubscriber());
+         station.addExtremeObserver(bwh.weatherDataSubscriber());
+         station.addTemperatureObserver(dwh.weatherDataSubscriber());
+         station.addHumidityObserver(dwh.weatherDataSubscriber());
+         station.addBarometerObserver(dwh.weatherDataSubscriber());
+         station.addCalculatedObserver(dwh.weatherDataSubscriber());
+         station.addExtremeObserver(dwh.weatherDataSubscriber());
          //Set up the HTTP Server
          HttpServer httpserver  =
                   HttpServer.create(new InetSocketAddress(8000),100);
-         httpserver.createContext("/",wdh);
+         //httpserver.createContext("/",wdh);
+         httpserver.createContext("/", bwh);
+         httpserver.createContext("/daily", dwh);
          httpserver.setExecutor(null);
          httpserver.start();
       }
