62343c60a25cd35e2a077cfb63c3ccfbda3af40f
Lou Rosas <lourosas@gmail.com>
2018-04-14 11:49:30 -0700

    04/14/2018

---
+++ RaspberryPiWeatherStationCode/rosas/lou/weatherclasses/ArchiveDisplayHandler.java
@@ -0,0 +1,105 @@
+/////////////////////////////////////////////////////////////////////
+/*
+<GNU Stuff to go here>
+*/
+/////////////////////////////////////////////////////////////////////
+package rosas.lou.weatherclasses;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import com.sun.net.httpserver.*;
+import rosas.lou.weatherclasses.*;
+
+public class ArchiveDisplayHandler extends BaseWeatherHandler
+implements HttpHandler{
+
+   private static String[] months = {"January", "February",
+   "March", "April", "May", "June", "July", "August", "September",
+   "October", "November", "December"};
+
+   private static String[] years = {"2017", "2018"};
+
+   //
+   //Implemtation of the HttpHandler interface
+   //
+   public void handle(HttpExchange exchange){
+      StringBuffer response = new StringBuffer();
+
+      try{
+         response.append(this.setUpHeader());
+         response.append(this.setUpBody()); 
+         exchange.sendResponseHeaders(200, response.length());
+         OutputStream os = exchange.getResponseBody();
+         os.write(response.toString().getBytes());
+         os.close();
+      }
+      catch(IOException ioe){ ioe.printStackTrace(); }
+   }
+
+   //*****************Private Methods********************************
+   //
+   //
+   //
+   private String setUpBody(){
+      Calendar now = Calendar.getInstance();
+      int month    = now.get(Calendar.MONTH);
+      int day      = now.get(Calendar.DATE);
+      int year     = now.get(Calendar.YEAR);
+      String theMonth = months[month];
+      StringBuffer body = new StringBuffer();
+      body.append("<body><h1>Try This Archiving</h1>");
+      body.append("<p>Make the attempt</p>");
+      body.append("\n<form action=");
+      body.append("\"http://68.98.39.148:8000/archive\" ");
+      body.append("method=\"GET\" ");
+      body.append("enctype=\"multipart/form-data\">");
+      //Month
+      body.append("\n<select value = \"month\">");
+      for(int i = 0; i < months.length; i++){
+         body.append("\n<option value = \""+months[i] +"\"");
+         if(i == month){
+            body.append(" selected");
+         }
+         body.append(">" + months[i] + "</option>");
+      }
+      body.append("\n</select>");
+      //Day
+      body.append("\n<select value = \"day\">");
+      for(int i = 0; i < 31; i++){
+         body.append("\n<option value = \"" + (i+1) + "\"");
+         if((i+1) == day){
+            body.append(" selected");
+         }
+         body.append(">" + (i+1) + "</option>");
+      }
+      body.append("\n</select>");
+      //Year
+      body.append("\n<select value = \"year\">");
+      for(int i = 0; i < years.length; i++){
+         body.append("\n<option value = \""+years[i]+"\"");
+         String value = "" + year;
+         if(value.equals(years[i])){
+            body.append(" selected");
+         }
+         body.append(">" + years[i] + "</option>");
+      }
+      body.append("\n<select>&nbsp;&nbsp;&nbsp;");
+      //Select Button
+      body.append("<input type=\"submit\" value=\"View\"/>");
+      body.append("\n</form>");
+      body.append("</body></html>");
+      return body.toString();
+   }
+
+   //
+   //
+   //
+   private String setUpHeader(){
+      StringBuffer header = new StringBuffer();
+      header.append("<html><head><title>Archived Inquiry");
+      header.append("</title></head>");
+      return header.toString();
+   }
+}
