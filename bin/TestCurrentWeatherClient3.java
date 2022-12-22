9c91449b97a9c833c16eeddc2647846b3d6c2e9f
lourosas <lourosas@gmail.com>
2022-12-22 08:24:44 -0700

    12-22-22-00

--- bin/TestCurrentWeatherClient3.java
+++ bin/TestCurrentWeatherClient3.java
@@ -13,16 +13,20 @@ public class TestCurrentWeatherClient3{
 
    public TestCurrentWeatherClient3(){
       System.out.println("Hello World");
+      //Weather Client
       CurrentWeatherClient cwc = new CurrentWeatherClient();
 
+      //Controller
       CurrentWeatherObservationPostController cwcontroller =
                         new CurrentWeatherObservationPostController();
+      //Model
       WeatherClientDataSubscriber wds =
                                   new CurrentWeatherObservationPost();
       cwc.addSubscriber(wds);
 
       cwcontroller.addModel((CurrentWeatherObservationPost)wds);
 
+      //View
       CurrentWeatherView wv = new CurrentWeatherObservationPostView(
                                             "Weather View",
                                             cwcontroller);
@@ -35,6 +39,8 @@ public class TestCurrentWeatherClient3{
       wds.addPublisher(iwdc);
       Thread thread2 = new Thread(iwdc);
       thread2.start();
+      Thread thread3 = new Thread((CurrentWeatherObservationPost)wds);
+      thread3.start();
    }
 }
 //////////////////////////////////////////////////////////////////////
